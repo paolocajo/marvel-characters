94ffcc83418ab9a117c9d73ab4682810f8a4718b
Paolo Martín Cajo Flores <paolocajo09@gmail.com>
2023-03-16 13:19:20 -0500

    Initial commit

---
+++ app/helpers/marvel_api.js
@@ -0,0 +1,10 @@
+// https://developer.marvel.com/
+
+const API_KEY = ``,
+  API_MARVEL = `https://gateway.marvel.com:443/v1/public`,
+  CHARACTERS = `${API_MARVEL}/characters?`,
+  CHARACTER_ID = `${API_MARVEL}/characters/`,
+  CHARACTER_NAME = `${API_MARVEL}/characters?name=`;
+CHARACTER_STARTS_WITH = `${API_MARVEL}/characters?nameStartsWith=`;
+
+export default {};
