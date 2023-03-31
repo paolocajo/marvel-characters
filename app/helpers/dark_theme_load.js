c8965efbb98478e71542c87e686a26eb6673f806
Paolo Martín Cajo Flores <paolocajo09@gmail.com>
2023-03-31 18:37:26 -0500

    Fix default theme

--- app/helpers/dark_theme_load.js
+++ app/helpers/dark_theme_load.js
@@ -1,6 +1,7 @@
 import { options } from "./dark_theme_options.js";
 export function darkThemeLoad() {
-  if (localStorage.getItem("theme") === null) ls.setItem("theme", "light");
+  if (localStorage.getItem("theme") === null)
+    localStorage.setItem("theme", "light");
   if (localStorage.getItem("theme") === "light") options.lightMode();
   if (localStorage.getItem("theme") === "dark") options.darkMode();
 }
